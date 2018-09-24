3f690eb349174b6c042a46ef383d048b4b5027f6
gabbyk09 <43526796+gabbyk09@users.noreply.github.com>
2018-09-23 20:07:16 -0400

    Add files via upload

---
+++ P2T1_SalesPrediction_GabrielleMcDougald.py
@@ -0,0 +1,13 @@
+# My first sales prediction program
+# 09/23/18
+# CTI-110 P2T1 - Sales Prediction
+# Gabrielle McDougald
+
+# Get the projected total sales.
+total_sales = float(input('Enter the projected sales: '))
+# Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+# Display the profit.
+print ( ' The profit is 2,300.00 ' )
+
+
